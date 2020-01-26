afaac80600a75e40cc2b10516c833346b301e5e6
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-01-26 11:19:58 +0100

    adds the GeojsonWfsPage

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ThemeProvider, GlobalStyle } from '@datapunt/asc-ui'
 import 'leaflet/dist/leaflet.css'
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, useHistory } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 
-const App = () => (
-  <ThemeProvider>
-    <GlobalStyle />
-    <Router>
-      {Object.entries(AppRoutes).map(([key, options]) => (
-        <Route key={key} {...options} />
-      ))}
-    </Router>
-  </ThemeProvider>
-)
+const defaultRoute = '/geojson-wfs' // '/';
+
+const App = () => {
+  useEffect(() => {
+    window.history.pushState({}, '', defaultRoute)
+  }, [])
+
+  return (
+    <ThemeProvider>
+      <GlobalStyle />
+      <Router>
+        {Object.entries(AppRoutes).map(([key, options]) => (
+          <Route key={key} {...options} />
+        ))}
+      </Router>
+    </ThemeProvider>
+  )
+}
 
 export default App
--- example/src/AppRoutes.tsx
+++ example/src/AppRoutes.tsx
@@ -1,11 +1,16 @@
 import HomePage from './Pages/HomePage'
 import GeocoderPage from './Pages/GeocoderPage'
+import GeojsonWfsPage from './Pages/GeojsonWfsPage'
 
 const AppRoutes = {
   HOME: { path: '/', component: HomePage, exact: true },
   GEOCODER: {
     path: '/geocoder',
     component: GeocoderPage,
+  },
+  GEOJSON_WFS: {
+    path: '/geojson-wfs',
+    component: GeojsonWfsPage,
   },
 }
 
