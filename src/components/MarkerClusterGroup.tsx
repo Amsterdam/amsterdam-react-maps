001054b1df2e901c0978fc0a050d1b4bd9f3738e
Chris van Mook <chris@chrisvanmook.com>
2020-05-22 17:18:08 +0200

    Bumped version

--- src/components/MarkerClusterGroup.tsx
+++ src/components/MarkerClusterGroup.tsx
@@ -9,12 +9,10 @@ import L, {
 import 'leaflet.markercluster'
 import { createGlobalStyle } from 'styled-components'
 import { defaultIcon } from '../icons'
-// @ts-ignore
-import clusterIcon from '../assets/icons/cluster-icon.svg'
 
 const Styles = createGlobalStyle`
   .arm__icon--clustergroup-default {
-    background-image: url(${clusterIcon});
+    background-image: url("data:image/svg+xml,%3Csvg width='39' height='39' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Cdefs%3E%3Ccircle id='a' cx='17' cy='17' r='16'/%3E%3Ccircle id='b' cx='16' cy='16' r='16'/%3E%3Cmask id='c' maskContentUnits='userSpaceOnUse' maskUnits='objectBoundingBox' x='-3' y='-3' width='38' height='38'%3E%3Cpath fill='%23fff' d='M-2-2h38v38H-2z'/%3E%3Cuse xlink:href='%23a'/%3E%3C/mask%3E%3Cmask id='d' maskContentUnits='userSpaceOnUse' maskUnits='objectBoundingBox' x='-3' y='-3' width='38' height='38'%3E%3Cpath fill='%23fff' d='M-3-3h38v38H-3z'/%3E%3Cuse xlink:href='%23b'/%3E%3C/mask%3E%3C/defs%3E%3Cg transform='translate(3 3)' fill='none' fill-rule='evenodd'%3E%3Cuse stroke='%23666' mask='url(%23c)' stroke-width='6' xlink:href='%23a'/%3E%3Cuse stroke='%23FFF' mask='url(%23d)' stroke-width='6' xlink:href='%23b'/%3E%3Ccircle fill='%23EC0000' cx='16' cy='16' r='16'/%3E%3C/g%3E%3C/svg%3E");
     background-size: 100%;
     color: #fff;
     display: flex;
