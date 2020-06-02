c7731a71579df571adaa98d28ed56a450f38757f
Chris van Mook <chris@chrisvanmook.com>
2020-06-02 10:49:32 +0200

    Added BaseLayerToggle component

--- example/src/pages/MapBaseLayers.tsx
+++ example/src/pages/MapBaseLayers.tsx
@@ -1,99 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Select, ViewerContainer } from '@datapunt/asc-ui'
+import { ViewerContainer } from '@datapunt/asc-ui'
 import Controls from '../../../src/components/Zoom'
-import BaseLayer from '../../../src/components/BaseLayer'
 import Map from '../../../src/components/Map'
-import {
-  MapLayer,
-  DEFAULT_AMSTERDAM_LAYERS,
-  AERIAL_AMSTERDAM_LAYERS,
-} from '../../../src/constants'
-import BaseLayerToggle, {
-  BaseLayerType,
-} from '../../../src/components/BaseLayerToggle'
+import BaseLayerToggle from '../../../src/components/BaseLayerToggle'
 
 const StyledViewerContainer = styled(ViewerContainer)`
+  height: 100vh;
   z-index: 400;
 `
 
-const MapBaseLayers: React.FC = () => {
-  const [baseLayer, setBaseLayer] = useState(
-    DEFAULT_AMSTERDAM_LAYERS[0].urlTemplate,
-  )
-  const [toggleBaseLayerType, setToggleBaseLayerType] = useState(
-    BaseLayerType.Aerial,
-  )
-
-  const [panelOpen, setPanelOpen] = useState(false)
-
-  const handleChange = (
-    e: React.FormEvent<HTMLSelectElement>,
-    layers: MapLayer[],
-    type: BaseLayerType,
-  ) => {
-    const { value } = e.currentTarget
-
-    const layer = layers.find(({ id }) => id === value)
-
-    if (layer) {
-      setBaseLayer(layer.urlTemplate)
-      // The opposite base layer will be set as type for the BaseLayerToggle
-      setToggleBaseLayerType(
-        type === BaseLayerType.Aerial
-          ? BaseLayerType.Topo
-          : BaseLayerType.Aerial,
-      )
-    }
-  }
-
-  return (
-    <Map fullScreen>
-      <StyledViewerContainer
-        topLeft={
-          panelOpen ? (
-            <>
-              <Select
-                id="varianten"
-                label="Varianten"
-                onChange={(e) =>
-                  handleChange(e, DEFAULT_AMSTERDAM_LAYERS, BaseLayerType.Topo)
-                }
-              >
-                {DEFAULT_AMSTERDAM_LAYERS.map(({ id, label }) => (
-                  <option key={id} value={id}>
-                    {label}
-                  </option>
-                ))}
-              </Select>
-              <Select
-                id="luchtfotos"
-                label="Luchtfoto's"
-                onChange={(e) =>
-                  handleChange(e, AERIAL_AMSTERDAM_LAYERS, BaseLayerType.Aerial)
-                }
-              >
-                {AERIAL_AMSTERDAM_LAYERS.map(({ id, label }) => (
-                  <option key={id} value={id}>
-                    {label}
-                  </option>
-                ))}
-              </Select>
-            </>
-          ) : null
-        }
-        bottomLeft={
-          <BaseLayerToggle
-            type={toggleBaseLayerType}
-            open={panelOpen}
-            onClick={setPanelOpen}
-          />
-        }
-        bottomRight={<Controls />}
-      />
-      <BaseLayer baseLayer={baseLayer} />
-    </Map>
-  )
-}
+const MapBaseLayers: React.FC = () => (
+  <Map fullScreen>
+    <StyledViewerContainer
+      bottomLeft={<BaseLayerToggle />}
+      bottomRight={<Controls />}
+    />
+  </Map>
+)
 
 export default MapBaseLayers
