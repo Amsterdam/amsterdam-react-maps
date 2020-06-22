d215217c02e222ed61971c1cfbbaffec32e1aaad
Chris van Mook <chris@chrisvanmook.com>
2020-06-22 13:06:16 +0200

    Updated storybook to beta

--- stories/.storybook/main.js
+++ stories/.storybook/main.js
@@ -1,58 +1,13 @@
 const path = require('path')
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin')
 
-const packages = path.resolve(__dirname, '../../', 'packages')
-const stories = path.resolve(__dirname, '../', 'src')
 module.exports = {
-  stories: ['../src/**/*.stories.(tsx|mdx)'],
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: [
-    '@storybook/addon-links/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-a11y/register',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-docs/register',
-    '@storybook/addon-viewport/register',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport',
   ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(stories|story)\.mdx$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-transform-react-jsx'],
-            presets: ['@babel/preset-env'],
-          },
-        },
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            compilers: [createCompiler({})],
-          },
-        },
-      ],
-    })
-
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      include: [stories, packages],
-      use: [
-        {
-          loader: 'ts-loader',
-        },
-      ],
-    })
-
-    config.resolve.extensions.push('.ts', '.tsx')
-
-    // Resolve every package to it's src directory
-    Object.assign(config.resolve.alias, {
-      '@datapunt/arm-core': path.join(packages, 'arm-core', '/src'),
-      '@datapunt/arm-cluster': path.join(packages, 'arm-cluster', '/src'),
-      '@datapunt/arm-draw': path.join(packages, 'arm-draw', '/src'),
-      '@datapunt/arm-nontiled': path.join(packages, 'arm-nontiled', '/src'),
-    })
-
-    return config
-  },
 }
