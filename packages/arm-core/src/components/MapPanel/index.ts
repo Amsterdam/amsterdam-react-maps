d4ee5b8a6df603299cbbd1b3fb1cdd21bcd3a76b
Jon Koops <jonkoops@gmail.com>
2020-07-02 15:40:02 +0200

    Export modules from MapPanel directly

--- packages/arm-core/src/components/MapPanel/index.ts
+++ packages/arm-core/src/components/MapPanel/index.ts
@@ -1,9 +1,7 @@
-import * as mapPanelConstants from './constants'
-
+export * as mapPanelConstants from './constants'
 export { default as MapPanel } from './MapPanel'
 export { default as MapPanelContent } from './MapPanelContent'
 export { default as MapPanelContext } from './MapPanelContext'
 export { default as MapPanelDrawer } from './MapPanelDrawer'
 export { default as MapPanelLegendButton } from './MapPanelLegendButton'
 export { default as MapPanelProvider } from './MapPanelProvider'
-export { mapPanelConstants }
--- packages/arm-core/src/index.ts
+++ packages/arm-core/src/index.ts
@@ -1,26 +1,25 @@
-import * as mapPanelComponents from './components/MapPanel'
 import * as constants from './constants'
 import * as icons from './icons'
 
-export { default as Map } from './components/Map'
-export { default as GeoJSONLayer } from './components/GeoJSONLayer'
-export { default as Marker } from './components/Marker'
-export { default as Zoom } from './components/Zoom'
-export { default as Scale } from './components/Scale'
-export { default as WfsLayer } from './components/WfsLayer'
 export { default as BaseLayer } from './components/BaseLayer'
 export { default as BaseLayerToggle } from './components/BaseLayerToggle'
 export { default as ControlButton } from './components/ControlButton'
-export { default as ViewerContainer } from './components/ViewerContainer'
+export { default as GeoJSONLayer } from './components/GeoJSONLayer'
+export { default as Map } from './components/Map'
+export * from './components/MapPanel'
+export { default as Marker } from './components/Marker'
 export {
   default as RDGeoJSON,
   defaultStyle as rdGeoJSONDefaultStyle,
 } from './components/RDGeoJSON'
+export { default as Scale } from './components/Scale'
+export { default as ViewerContainer } from './components/ViewerContainer'
+export { default as WfsLayer } from './components/WfsLayer'
+export { default as Zoom } from './components/Zoom'
+export { default as fetchWithAbort } from './utils/fetchWithAbort'
+export { default as getBBox } from './utils/getBBox'
 export { default as getCrsRd } from './utils/getCrsRd'
 export { default as useGetAddressFromLatLng } from './utils/useGetAddressFromLatLng'
 export { default as usePanToLatLng } from './utils/usePanToLatLng'
 export { default as useStateRef } from './utils/useStateRef'
-export { default as fetchWithAbort } from './utils/fetchWithAbort'
-export { default as getBBox } from './utils/getBBox'
-
-export { constants, mapPanelComponents, icons }
+export { constants, icons }
