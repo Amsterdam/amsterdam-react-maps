a1e568dabce39e9e43f1188090b4b7eafef38b74
Jasper Swart <jasper.g.swart@gmail.com>
2021-05-17 12:46:43 +0200

    added click tests

--- packages/arm-core/src/components/Zoom.test.tsx
+++ packages/arm-core/src/components/Zoom.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import { useMapInstance } from '@amsterdam/react-maps'
 import { mocked } from 'ts-jest/utils'
 
@@ -9,15 +9,15 @@ jest.mock('@amsterdam/react-maps')
 const mockedUseMapInstance = mocked(useMapInstance)
 
 describe('Zoom', () => {
-  let getZoom: jest.Mock
+  //   let getZoom: jest.Mock
   let setZoom: jest.Mock
 
   beforeEach(() => {
-    getZoom = jest.fn()
+    // getZoom = jest.fn()
     setZoom = jest.fn()
     // @ts-ignore
     mockedUseMapInstance.mockImplementation(() => ({
-      getZoom,
+      getZoom: () => 10,
       setZoom,
     }))
   })
@@ -26,5 +26,15 @@ describe('Zoom', () => {
     render(<Zoom />)
 
     expect(screen.queryByTestId('zoom')).not.toBeNull()
+    expect(screen.queryByTestId('zoomIn')).not.toBeNull()
+    expect(screen.queryByTestId('zoomOut')).not.toBeNull()
+
+    fireEvent.click(screen.getByTestId('zoomIn'))
+
+    expect(setZoom).toHaveBeenCalledWith(11)
+
+    fireEvent.click(screen.getByTestId('zoomOut'))
+
+    expect(setZoom).toHaveBeenCalledWith(9)
   })
 })
