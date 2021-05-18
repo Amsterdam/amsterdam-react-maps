53af9e7cc85fb548cb483dd457f47cde2d1367b5
Jasper Swart <jasper.g.swart@gmail.com>
2021-05-18 14:54:01 +0200

    added more test cases

--- packages/arm-core/src/components/ViewerContainer.test.tsx
+++ packages/arm-core/src/components/ViewerContainer.test.tsx
@@ -1,20 +1,34 @@
 import { render, screen } from '@testing-library/react'
+// import { themeSpacing } from '@amsterdam/asc-ui'
 
 import ViewerContainer from './ViewerContainer'
 
 describe('ViewerContainer', () => {
-  beforeEach(() => {
-    // @ts-ignore
-  })
-
-  afterEach(() => {
-    jest.resetAllMocks()
-  })
-
   it('should render correctly', () => {
-    render(<ViewerContainer />)
-    // @TODO fix render
+    render(
+      <ViewerContainer
+        topLeft="top left content"
+        topRight="top right content"
+        bottomLeft="bottom left content"
+        bottomRight="bottom right content"
+      />,
+    )
 
-    expect(screen.queryByTestId('container')).not.toBeNull()
+    screen.debug()
+
+    expect(screen.queryByTestId('container')).toBeInTheDocument()
+
+    expect(screen.queryByTestId('topLeft')).toBeInTheDocument()
+    expect(screen.queryByTestId('topRight')).toBeInTheDocument()
+    expect(screen.queryByTestId('bottomLeft')).toBeInTheDocument()
+    expect(screen.queryByTestId('bottomRight')).toBeInTheDocument()
+
+    expect(screen.queryByText('top left content')).toBeInTheDocument()
+    expect(screen.queryByText('top right content')).toBeInTheDocument()
+    expect(screen.queryByText('bottom left content')).toBeInTheDocument()
+    expect(screen.queryByText('bottom right content')).toBeInTheDocument()
+
+    // @ts-ignore
+    // expect(screen.getByTestId('topLeft')).toHaveStyleRule('left', '16px')
   })
 })
