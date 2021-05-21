f9ebed2ed905da1168574b61889012759eaaf9fe
Jasper Swart <jasper.g.swart@gmail.com>
2021-05-21 15:14:29 +0200

    added test

--- packages/arm-core/src/components/GeoJSONLayer.test.tsx
+++ packages/arm-core/src/components/GeoJSONLayer.test.tsx
@@ -1,7 +1,11 @@
+import fetchMock from 'jest-fetch-mock'
 import { render } from '@testing-library/react'
+import { renderHook } from '@testing-library/react-hooks'
 // import { useMapInstance } from '@amsterdam/react-maps'
 // import { mocked } from 'ts-jest/utils'
+import fetchWithAbort from '../utils/fetchWithAbort'
 
+// import GeoJSONMock from './GeoJSONMock.json'
 import GeoJSONLayer from './GeoJSONLayer'
 
 // jest.mock('@amsterdam/react-maps')
@@ -11,17 +15,25 @@ import GeoJSONLayer from './GeoJSONLayer'
 describe('GeoJSONLayer', () => {
   beforeEach(() => {
     // @ts-ignore
-    // mockedUseMapInstance.mockImplementation(() => ({}))
   })
 
   afterEach(() => {
+    fetchMock.resetMocks()
     jest.resetAllMocks()
   })
 
   it('should render correctly', () => {
+    // const json = { foo: 'bar' }
+    fetchMock.mockResponse(JSON.stringify({}))
+
+    // @ts-ignore
+    const [request, controller] = renderHook(() =>
+      fetchWithAbort('http://go.nl'),
+    )
+    console.log('', request, controller)
+
     const options = {}
     render(<GeoJSONLayer url="http://go.nl" options={options} />)
-    // @TODO fix render
 
     expect(1).toBe(1)
     // expect(screen.queryByTestId('marker')).not.toBeNull()
