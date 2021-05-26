a3eda3ce56d707b07ae25dd48fa69294251a8336
Jasper Swart <jasper.g.swart@gmail.com>
2021-05-26 14:11:35 +0200

    fixed unit test for Marker

--- packages/arm-core/src/components/Marker.test.tsx
+++ packages/arm-core/src/components/Marker.test.tsx
@@ -1,31 +1,19 @@
-import { render, screen } from '@testing-library/react'
-import { useMapInstance } from '@amsterdam/react-maps'
-import { mocked } from 'ts-jest/utils'
-
+import { render } from '@testing-library/react'
+import { Map } from '@amsterdam/arm-core'
 import Marker from './Marker'
 
-jest.mock('@amsterdam/react-maps')
-
-const mockedUseMapInstance = mocked(useMapInstance)
-
 describe('Marker', () => {
-  beforeEach(() => {
-    // @ts-ignore
-    mockedUseMapInstance.mockImplementation(() => ({}))
-  })
-
   afterEach(() => {
     jest.resetAllMocks()
   })
 
   it('should render correctly', () => {
-    render(
-      <div>
+    const { container } = render(
+      <Map>
         <Marker latLng={{ lat: 52, lng: 4 }} />
-      </div>,
+      </Map>,
     )
-    // screen.debug()
 
-    expect(screen.queryByTestId('marker')).toBeInTheDocument()
+    expect(container.querySelector('.leaflet-marker-icon')).toBeInTheDocument()
   })
 })
