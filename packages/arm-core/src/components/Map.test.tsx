fc3fc2388c4f19803e91f04680f8ed153354b9a9
Jasper Swart <jasper.g.swart@gmail.com>
2021-06-09 17:03:43 +0200

    added test for Map compoent

--- packages/arm-core/src/components/Map.test.tsx
+++ packages/arm-core/src/components/Map.test.tsx
@@ -6,13 +6,20 @@ describe('Map', () => {
     jest.resetAllMocks()
   })
 
-  it('should render correctly', () => {
+  it('should render default', () => {
     const { container } = render(<Map>map content</Map>)
 
-    screen.debug()
+    expect(screen.queryByText('map content')).toBeInTheDocument()
 
     expect(
       container.querySelector('.leaflet-container .leaflet-map-pane'),
     ).toBeInTheDocument()
+  })
+
+  it('should render fullScreen', () => {
+    const { container } = render(<Map fullScreen>map content</Map>)
+
+    expect(container.firstChild).toHaveStyleRule('width', '100%')
+    expect(container.firstChild).toHaveStyleRule('height', '100%')
   })
 })
