ac6eb046752718fed646a88ec8da9405feccd160
Jasper Swart <jasper.g.swart@gmail.com>
2021-06-11 12:58:28 +0200

    added first part of test of GeoJSONLayer

---
+++ packages/arm-core/src/components/GeoJSONLayer.test.tsx
@@ -0,0 +1,23 @@
+// import { render } from '@testing-library/react'
+// import { renderHook } from '@testing-library/react-hooks'
+// import { Map } from '@amsterdam/arm-core'
+// import fetchWithAbort from '../utils/fetchWithAbort'
+
+// import GeoJSONLayer from './GeoJSONLayer'
+
+describe('GeoJSONLayer', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should render correctly', () => {
+    // const { request, controller } = renderHook(() => fetchWithAbort('yooooo'))
+    // console.log('test', request, controller)
+    // const { container } = render(
+    //   <Map>
+    //     <GeoJSONLayer url="this_url" />
+    //   </Map>,
+    // )
+    // expect(container.firstChild).toBeInTheDocument()
+  })
+})
