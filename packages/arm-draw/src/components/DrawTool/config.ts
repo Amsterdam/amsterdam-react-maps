53b626499db4a9adcf913470709d92335aa04708
Jasper Swart <jasper.g.swart@gmail.com>
2021-09-27 14:25:21 +0200

    removed code

--- packages/arm-draw/src/components/DrawTool/config.ts
+++
@@ -1,4 +0,0 @@
-export const TOGGLE_BUTTON_TITLE = 'Open de teken-tool'
-export const POLYLINE_BUTTON_TITLE = 'Teken een lijn om de afstand te meten'
-export const POLYGON_BUTTON_TITLE = 'Teken een vlak om data te selecteren'
-export const REMOVE_BUTTON_TITLE = 'Verwijder geselecteerde tekening'
