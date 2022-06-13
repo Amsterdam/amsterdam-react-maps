202611305fbe37ba269554989848241007a980d4
Jasper Swart <jasper.g.swart@gmail.com>
2022-06-13 21:43:31 +0200

    added useMapInstance test

---
+++ packages/arm-core/src/components/useMapInstance.test.tsx
@@ -0,0 +1,30 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { Map } from 'leaflet'
+import { FunctionComponent } from 'react'
+import MapContext from './MapContext'
+import { useMapInstance } from '../index'
+
+describe('useMapInstance', () => {
+  it('should use the map instance', () => {
+    const mockInstance = { foo: 'bar' } as unknown as Map
+    const wrapper: FunctionComponent = ({ children }) => (
+      <MapContext.Provider
+        value={{
+          mapInstance: mockInstance,
+        }}
+      >
+        {children}
+      </MapContext.Provider>
+    )
+
+    const { result } = renderHook(() => useMapInstance(), { wrapper })
+    console.log('---------------------------------------,', result.current)
+
+    expect(result.current).toEqual(mockInstance)
+  })
+
+  it("should throw an exception if the provider doesn't exist", () => {
+    const { result } = renderHook(() => useMapInstance())
+    expect(result.error).toBeDefined()
+  })
+})
