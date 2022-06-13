338a881c88b9ce4c805dc29d066b8db51e0436f5
Jasper Swart <jasper.g.swart@gmail.com>
2022-06-13 19:03:27 +0200

    added GoJSON test

---
+++ packages/arm-core/src/components/GeoJSON.test.tsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react'
+import { GeoJSONOptions } from 'leaflet'
+import { GeoJSON as GeoJSONType } from 'geojson'
+import Map from './Map'
+import { GeoJSON } from '../index'
+
+describe('Marker', () => {
+  const geojsonFeature: GeoJSONType = {
+    type: 'Feature',
+    properties: {
+      name: 'Amsterdam',
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [52, 4],
+    },
+  }
+
+  const mockOptions: GeoJSONOptions = {}
+
+  it('should render correctly', () => {
+    const { container } = render(
+      <Map>
+        <GeoJSON args={[geojsonFeature]} options={mockOptions} />
+      </Map>,
+    )
+
+    expect(container.querySelectorAll(' .leaflet-map-pane').length).toBe(1)
+  })
+})
