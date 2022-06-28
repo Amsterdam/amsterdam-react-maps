1f586f2775adedb480dd1e3fb554231c28ddb1c8
Jasper Swart <jasper.g.swart@gmail.com>
2022-06-28 15:14:14 +0200

    added tabindex

--- packages/arm-core/src/components/ViewerContainer.tsx
+++ packages/arm-core/src/components/ViewerContainer.tsx
@@ -6,6 +6,10 @@ type Props = {
   topRight?: React.ReactNode
   bottomLeft?: React.ReactNode
   bottomRight?: React.ReactNode
+  tabIndexTopLeft?: number
+  tabIndexTopRight?: number
+  tabIndexBottomLeft?: number
+  tabIndexBottomRight?: number
   metaData?: Array<string>
 }
 
@@ -68,21 +72,25 @@ const ViewerContainer: React.FC<Props> = ({
   topLeft,
   topRight,
   bottomRight,
+  tabIndexTopLeft,
+  tabIndexTopRight,
+  tabIndexBottomLeft,
+  tabIndexBottomRight,
   metaData,
   ...otherProps
 }) => {
   return (
     <ViewerContainerWrapper {...otherProps} data-testid="container">
-      <ViewerContainerItem position="top-left" data-testid="topLeft">
+      <ViewerContainerItem position="top-left" tabIndex={tabIndexTopLeft || 1} data-testid="topLeft">
         {topLeft}
       </ViewerContainerItem>
-      <ViewerContainerItem position="top-right" data-testid="topRight">
+      <ViewerContainerItem position="top-right" tabIndex={tabIndexTopRight || 2} data-testid="topRight">
         {topRight}
       </ViewerContainerItem>
-      <ViewerContainerItem position="bottom-left" data-testid="bottomLeft">
+      <ViewerContainerItem position="bottom-left" tabIndex={tabIndexBottomLeft || 3} data-testid="bottomLeft">
         {bottomLeft}
       </ViewerContainerItem>
-      <ViewerContainerItem position="bottom-right" data-testid="bottomRight">
+      <ViewerContainerItem position="bottom-right" tabIndex={tabIndexBottomRight || 4} data-testid="bottomRight">
         {bottomRight}
         {metaData && (
           <div>
