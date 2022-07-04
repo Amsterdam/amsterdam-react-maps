20923221741411b091aae19dbf003864a4328946
Jasper Swart <jasper.g.swart@gmail.com>
2022-07-04 17:26:00 +0200

    Delete gh-pages.js

--- scripts/gh-pages.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line
-const ghpages = require('gh-pages')
-
-ghpages.publish('stories/lib', (err) => {
-  // eslint-disable-next-line no-console
-  console.warn(err)
-  process.exit(1)
-})
